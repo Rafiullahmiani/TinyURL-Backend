2b4b6d4908577af5c2cb55c76d6343b5e9d8d17f
Rafi ullah <rafiullahmiani1509@gmail.com>
2025-09-01 06:53:43 +0500

    Update mongodb.js

--- Utils/mongodb.js
+++ Utils/mongodb.js
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 export const ConnectMongoDb = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL || "");
-    console.log("✅ DB connected");
+    console.log(" DB connected");
   } catch (err) {
-    console.error("❌ DB Connection Error:", err.message); // <-- actual error message milega
+    console.error(" DB Connection Error:", err.message); // <-- actual error message milega
     process.exit(1); // optional: server band kar do agar DB connect na ho
   }
 };
+
