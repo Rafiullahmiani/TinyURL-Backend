b08ca72b6fbc4edbcc1b9fb93f3a1f29588a0232
Rafi ullah <rafiullahmiani1509@gmail.com>
2025-09-04 19:56:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,12 @@ app.use(express.json());
 
 app.use("/url", URLRoute);
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
+
 
 
